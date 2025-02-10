1e18697a4c606a44c3d324107b316fe862d6f454
Balram Rajbanshi <balramrajbanshi4769@gmail.com>
2025-02-10 17:10:10 +0500

    backend day1 express

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require("express");
+const app = express();
+let PORT = process.env.PORT || 5000;
+console.log('Final port is',PORT);
+
+
+
+app.get("/", (req, res) => {
+  res.send("Hello world ! Welcome to express world");
+});
+
+app.get("/welcome", (req, res) => {
+  res.send("Welcome Express on backend development");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT:${PORT}`);
+});
