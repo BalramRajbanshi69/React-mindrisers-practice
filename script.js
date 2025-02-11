3e83879842be54ee4f6258b5496fa5a334d63439
Balram Rajbanshi <balramrajbanshi4769@gmail.com>
2025-02-11 13:37:22 +0500

    dynamic ejs change

--- script.js
+++ script.js
@@ -8,12 +8,12 @@ app.set('view engine','ejs');
 // render instead of send to view in browser
 // make views folder and index.ejs file(here write your html code,css, js)
 app.get('/',(req,res)=>{
-  res.render('index')
+  res.render('index',{name:'BALRAM RAJBANSHI'})
 })
 
 
 app.get("/about", (req, res) => {
-  res.render("about");
+  res.render("about",{firstName:'BALRAM',lastName:'RAJBANSHI'});
 });
 
 
