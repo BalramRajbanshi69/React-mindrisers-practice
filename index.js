b39388cafb1c4ebd3561bac2502c97d00c6a170c
Balram Rajbanshi <balramrajbanshi4769@gmail.com>
2025-02-11 10:06:13 +0500

    req,res,next and middleware

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ const app = express();
 let PORT = process.env.PORT || 5000;
 console.log(PORT);
 
-
+// this is middleware , is a function that executes before routing /, /profile,/contact
+// req=> is client/user data sending to server
+// res=> is server response to client/user
+// next => must call next() to continue request flow
+// if not used next(); request flow will stop and cannot run the routing process
+app.use((req,res,next)=>{
+  next();
+})
 
 app.get('/',(req,res)=>{
   res.send("Hello world");
@@ -12,9 +19,9 @@ app.get('/',(req,res)=>{
 
 
 app.get("/welcome", (req, res) => {
-  // res.send("Welcome to Express Backend Development");
+  res.send("Welcome to Express Backend Development");
   // res.status(404).send("Not Found");
-  res.sendStatus(404);
+  // res.sendStatus(404);
 });
 
 app.listen(PORT,()=>{
